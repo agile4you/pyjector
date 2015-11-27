8ee13e388e7d5e4c3c3d8a231cd5e3b04d4471f8
Vassilis Papavassiliou <pav@geotagaeroview.com>
2015-11-27 18:10:19 +0200

    Updated README

--- examples.py
+++ examples.py
@@ -6,4 +6,28 @@ from pyjector import Injector
 
 provider = Injector()
 
-print provider.version
+fixtures = {chr(i): i for i in range(97, 120)}
+
+class DB(object):
+    """A fake db class
+    """
+
+    def __init__(self, **fixtures):
+        self.__data = fixtures
+
+    def retrieve(self, pk):
+        return self.__data.get(pk)
+
+
+print fixtures
+
+provider.register_callable(DB(**fixtures), 'db')
+
+
+@provider.inject('db')
+def char_to_integer(db, ch):
+    num = db.retrieve(ch)
+    return num
+
+
+print char_to_integer(ch='c')
