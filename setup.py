f33414ad53570e6d4371c61d81d392fb7fd7206e
Vassilis Papavassiliou <pav@geotagaeroview.com>
2016-03-02 12:28:59 +0200

    Added version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs_open('README.rst', encoding='utf-8') as f:
 
 setup(
     name='pyjector',
-    version='0.0.1a2',
+    version='0.0.1a3',
     description=u"Python Depedency Injection utilities.",
     long_description=long_description,
     classifiers=[],
